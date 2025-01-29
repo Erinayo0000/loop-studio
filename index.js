7804ba2bc49dc0337ad0c5ee12230761dcfd06c1
Olusanmi Pamilerin kehinde <pamilerinolusanmi@gmail.com>
2025-01-29 23:18:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("offCanvasNav").classList.add("open");
+}
+
+function closeNav() {
+  document.getElementById("offCanvasNav").classList.remove("open");
+}
